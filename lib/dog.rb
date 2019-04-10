c85bc3c0475035135c2241d4890877e34845d9df
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-10 17:01:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
-class Dog 
-  def name = (dog_name)
-    @this_dog_name = dog_name
-  end
-  
-  def name
-   @this_dog_name
-  end
-
-end 
+    class Dog
+     
+      def name(name)
+        @name = name
+      end
+     
+      def name
+        @name
+      end
+     
+      def name=(new_name)
+        @name = new_name
+      end
+     
+    end
 
 milou = Dog.new
 milou.name = "Milou"
